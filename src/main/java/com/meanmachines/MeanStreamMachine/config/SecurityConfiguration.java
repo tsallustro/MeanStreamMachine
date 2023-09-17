5c8a7a2fdb0cf9ae9f7fa5c79e83808d65f0bdc9
Tony S <tsallustro@outlook.com>
2023-09-17 16:23:04 -0400

    weed

--- src/main/java/com/meanmachines/MeanStreamMachine/config/SecurityConfiguration.java
+++ src/main/java/com/meanmachines/MeanStreamMachine/config/SecurityConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class SecurityConfiguration {
 
--- src/main/java/com/meanmachines/MeanStreamMachine/repositories/MediaRepository.java
+++ src/main/java/com/meanmachines/MeanStreamMachine/repositories/MediaRepository.java
@@ -3,7 +3,6 @@ package com.meanmachines.MeanStreamMachine.repositories;
 import com.meanmachines.MeanStreamMachine.model.dbentities.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface MediaRepository extends JpaRepository<Media, UUID> {
