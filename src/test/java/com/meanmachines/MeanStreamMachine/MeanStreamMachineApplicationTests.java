2130535a432100e152f7107e0031277004ca210b
Tony S <tsallustro@outlook.com>
2023-09-22 16:06:44 -0400

    add basic react stuff

--- src/test/java/com/meanmachines/MeanStreamMachine/MeanStreamMachineApplicationTests.java
+++ src/test/java/com/meanmachines/MeanStreamMachine/MeanStreamMachineApplicationTests.java
@@ -2,7 +2,9 @@ package com.meanmachines.MeanStreamMachine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("h2")
 @SpringBootTest
 class MeanStreamMachineApplicationTests {
 
