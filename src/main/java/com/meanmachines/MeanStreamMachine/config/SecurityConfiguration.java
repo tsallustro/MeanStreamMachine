805ee80f3063fd0e94a600d59eabcc4f3ad83c0b
Tony S <tsallustro@outlook.com>
2023-10-31 19:56:42 -0400

    auth

--- src/main/java/com/meanmachines/MeanStreamMachine/config/SecurityConfiguration.java
+++ src/main/java/com/meanmachines/MeanStreamMachine/config/SecurityConfiguration.java
@@ -2,23 +2,35 @@ package com.meanmachines.MeanStreamMachine.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
-                .httpBasic(withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .headers(header -> header.frameOptions(frameOptions -> frameOptions.disable()));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest()
+                .hasRole("USER"))
+                .formLogin(login -> login.loginPage("/login").permitAll());
+
         return http.build();
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder().username("user").password("{bcrypt}$2y$10$4/t2i8BLeq8A3UesUmNBSu5v8UGBwzckp9xng80Q88dboFON/njEm").roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password("{bcrypt}$2y$10$OjQY.QoeZnc1j/PXOwNbJ.4EApbRjur.mduAN/ZHhPT7CNVN1zeQW").roles("USER", "ADMIN").build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
 
 }
--- src/main/java/com/meanmachines/MeanStreamMachine/controller/WebController.java
+++ src/main/java/com/meanmachines/MeanStreamMachine/controller/WebController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
     @Autowired
     private MediaService mediaService;
-
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
     @RequestMapping(value = {"/","/home"})
     public String home(Model model) {
 
